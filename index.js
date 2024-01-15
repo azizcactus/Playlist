b41280c1377266d50a46822b4b7b77d9b41f6304
azizcactus <148267478+azizcactus@users.noreply.github.com>
2024-01-15 23:51:22 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+let songs = [
+    {
+        name: 'Runaway',
+        path: 'KWR.mp3',
+        artist: 'Kanye West',
+        cover: 'Kanye West.jpg'
+    },
+    {
+        name: 'I Know ?',
+        path: 'TSK.mp3',
+        artist: 'Travis Scott',
+        cover: 'Travis Scott.jpg'
+    },
+    {
+        name: 'Glock In My Lap',
+        path: '21SGIML.mp3',
+        artist: '21Savage',
+        cover: '21Savage.jpg'
+    },
+    {
+        name: 'Doja',
+        path: 'CCD.mp3',
+        artist: 'Central Cee',
+        cover: 'Central Cee.jpg'
+    },
+    {
+        name: 'Ham',
+        path: 'KH.mp3',
+        artist: 'Khontkar',
+        cover: 'Khontkar.jpg'
+    },
+    {
+        name: 'First Class',
+        path: 'CFC.mp3',
+        artist: 'Ceg',
+        cover: 'Ceg.jpg'
+    },
+    {
+        name: 'Pirana',
+        path: 'SP.mp3',
+        artist: 'Sehinsah',
+        cover: 'img/Sehinsah.jpg'
+    },
+]
